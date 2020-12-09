0d42c584fda36ab7d5307cc265b6789c4a09a336
johnhcody <john.haner.cody@gmail.com>
2020-12-09 15:47:57 +0700

    adding

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -21,8 +22,10 @@ mongoose
 // app.use(passport.initialize());
 // require('./config/passport')(passport);
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
