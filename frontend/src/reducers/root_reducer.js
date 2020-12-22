8eebeb393b0203033d27ac09999441ef7c8d0cdf
johnhcody <john.haner.cody@gmail.com>
2020-12-22 19:26:40 +0700

    new frontend

--- frontend/src/reducers/root_reducer.js
+++
@@ -1,9 +0,0 @@
-//import { combineReducers } from 'redux';
-import session from './session_reducer';
-import errors from './session_errors_reducer';
-
-
-export const RootReducer = {
-    session,
-    errors
-};
